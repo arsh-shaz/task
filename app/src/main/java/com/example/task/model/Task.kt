b6ba1839052e2535c37cb79e34f26ff1ac2bd579
arshaq2001 <mjmarshaq@gmail.com>
2024-05-10 21:57:23 +0530

    feat: added DAO, ROOM database

---
+++ app/src/main/java/com/example/task/model/Task.kt
@@ -0,0 +1,15 @@
+package com.example.task.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "tasks")
+@Parcelize
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val title:String,
+    val content:String
+): Parcelable
