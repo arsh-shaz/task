6e8137fae372ed23c0541c6cf3fbb67c2e881610
arshaq2001 <mjmarshaq@gmail.com>
2024-05-12 11:09:04 +0530

    fix: minor changes in ui

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.task
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.task.database.TaskDatabase
 import com.example.task.repository.TaskRepository
